a9215dbd10dcbef9036ee7f9220a36e117ca5340
jimmyzhang1995 <lonelydream95@gmail.com>
2019-11-15 13:22:24 -0800

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,19 @@ import * as serviceWorker from './serviceWorker';
 import SignIn from './components/auth/sign'
 import Home from './components/home/home';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import profile from './components/profile/profile';
 
 const routing = (
     <Router>
         <div>
             {/* <UserTest /> <DashBoard />
             {/* <Route exact path="/" component={App} /> */}
-            <Route exact path="/" component={Home} />
-            <Route path="/sign" component={SignIn} />
+            <Route path="/" component={Home} exact />
+            <Route path="/sign" component={SignIn} exact />
+
+            <Route path="/profile" component={profile} exact />
             {/* <Route path="/signup" component={SignUp} />  */}
+            {/* <Route path="/" component={Home} /> */}
             {/* <Profile /> */}
             {/* <Earning /> */}
             {/* <Manage /> */}
