ddc928c499cbe64136c527094345c20fa72c2dcd
pooldiver69 <fycai21@gmail.com>
2019-11-15 11:12:07 -0800

    add marterial icon

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-        <div className="home">
-          <Navbar />
-          <div className="main">
-            <Gallery />
-          </div>
-          <Footer />
+        // <div className="home">
+        //   <Navbar />
+        //   <div className="main">
+        //     <Gallery />
+        //   </div>
+        //   <Footer />
+        // </div>
+        <div>
+          Home
         </div>
     );
   }
